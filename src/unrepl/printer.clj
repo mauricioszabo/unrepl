123dd035750b674c5af7fe6592b11992009292d4
Maurício Szabo <mauricio.szabo@gmail.com>
2019-01-28 01:13:48 -0200

    Fixed unbalanced brackets.

--- src/unrepl/printer.clj
+++ src/unrepl/printer.clj
@@ -442,7 +442,7 @@
       (print-trusted-tag-lit-on write "unrepl/object"
                                 [(class x) (format "0x%x" (System/identityHashCode x)) (object-representation x)
                                  {:bean {unreachable (tagged-literal 'unrepl/... (*elide* (ElidedKVs. (bean x))))}
-                                  :pr-str (pr-str x)}}]
+                                  :pr-str (pr-str x)}]
                                 (sat-inc rem-depth))))) ; is very trusted
 
 (defn edn-str [x]
